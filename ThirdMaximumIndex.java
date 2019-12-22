5c0e38063c915f28805adfababa8ed36ce35343e
ichroman <ichromanrd@linecorp.com>
2019-12-22 23:22:37 +0700

    add comment in ThirdMaximumIndex

--- ThirdMaximumIndex.java
+++ ThirdMaximumIndex.java
@@ -9,29 +9,30 @@ public class ThirdMaximumIndex {
 	}
 	
 	public static int thirdMax(int[] nums) {
+		// define default max and min values
 		int max = Integer.MIN_VALUE, max2 = Integer.MIN_VALUE, max3 = Integer.MIN_VALUE, min = Integer.MAX_VALUE;
 		
+		// find the max and min value of these array
 		for (int ii = 0; ii < nums.length; ii++) {
 			max = max < nums[ii] ? nums[ii] : max;
 			min = min > nums[ii] ? nums[ii] : min;
 		}
 		
+		// find the second max
 		for (int ii = 0; ii < nums.length; ii++) {
 			int c = nums[ii];
-			if (c == max)
-				continue;
-			if (c > max2) 
+			if (c < max && c > max2) 
 				max2 = c > min && c < max ? c : max2;
 		}
 		
+		// find the third max
 		for (int ii = 0; ii < nums.length; ii++) {
 			int c = nums[ii];
-			if (c >= max2)
-				continue;
-			if (c > max3)
+			if (c < max2 && c > max3)
 				max3 = c;
 		}
 		
+		// assert if max2 and max3 sequentially lesser than previous max, if true then return max3 or return max instead
 		return max > max2 && max2 > max3 ? max3 : max;
 	}
 }
